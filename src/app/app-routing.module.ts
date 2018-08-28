fea1a2c142648c077cafef16eae813a02cfc0cec
Yuri <yuri@coddde.com>
2018-08-28 17:58:50 +0200

    Created an animation effects linked to the router
    guide on what has been done can be found at:
    https://medium.com/google-developer-experts/angular-supercharge-your-router-transitions-using-new-animation-features-v4-3-3eb341ede6c8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,10 @@ import { VisualizationComponent } from './visualization/visualization.component'
 import { AuthenticatedNavigationComponent } from './authenticated-navigation/authenticated-navigation.component';
 
 const routes:Routes = [
-  { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
-  { path: 'dashboard', component: VisualizationComponent },
-  { path: 'authenticated-navigation', component: AuthenticatedNavigationComponent },
-  { path:'auth', loadChildren:"./auth/auth.module#AuthModule"} //this path is loaded lazly, it means that it will look the auth.module file for the childrens paths there!
+  { path: '', redirectTo: '/auth/login', pathMatch: 'full', data: { state: 'login' } },
+  { path: 'dashboard', component: VisualizationComponent, data: { state: 'dashboard' }},
+  { path: 'authenticated-navigation', component: AuthenticatedNavigationComponent, data: { state: 'authenticatedNavigation' } },
+  { path: 'auth', loadChildren:"./auth/auth.module#AuthModule"} //this path is loaded lazly, it means that it will look the auth.module file for the childrens paths there!
 ]
 
 @NgModule({
